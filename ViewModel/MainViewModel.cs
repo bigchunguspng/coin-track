cf918f69ea3e1d860c98000d1edbb404a27a04ad
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2023-10-27 12:07:38 +0300

    Open main page by context menu

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -9,12 +9,18 @@ public class MainViewModel : NotifyPropertyChanged
 {
     public MainViewModel()
     {
+        var mainPage = new TopCurrencies();
+
+        TabBar = new TabBar(new ObservableCollection<Page>() { mainPage });
+
+        OpenMainPage = new RelayCommand(_ => TabBar.NewTab(mainPage));
+
         Instance = this;
-        
-        TabBar = new TabBar(new ObservableCollection<Page>() { new TopCurrencies() });
     }
 
     public static MainViewModel Instance { get; private set; } = null!;
 
     public TabBar TabBar { get; }
+
+    public RelayCommand OpenMainPage { get; }
 }
