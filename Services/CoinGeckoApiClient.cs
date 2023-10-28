8ce72907f1a9f2977c1c5a4e1d16a2b85beccb90
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2023-10-29 01:04:50 +0300

    CacheService.cs

--- Services/CoinGeckoApiClient.cs
+++ Services/CoinGeckoApiClient.cs
@@ -1,5 +1,5 @@
+using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -14,14 +14,16 @@ public class CoinGeckoApiClient
     // Cache / Update Frequency: every 45 seconds
 
     private const string BaseUrl = "https://api.coingecko.com/api/v3/";
-    private const string CoinsVsUsd6D = BaseUrl + "coins/markets?vs_currency=usd&precision=8";
+    private const string CoinsVsUSD = BaseUrl + "coins/markets?vs_currency=usd&precision=8";
 
-    private const string TopCoins = CoinsVsUsd6D + "&price_change_percentage=1h,24h,7d&per_page={0}";
-    private const string CoinById = CoinsVsUsd6D + "&price_change_percentage=1h,24h,7d,14d,30d,1y&ids={0}";
+    private const string TopCoins = CoinsVsUSD + "&price_change_percentage=1h,24h,7d&per_page={0}";
+    private const string CoinById = CoinsVsUSD + "&price_change_percentage=1h,24h,7d,14d,30d,1y&ids={0}";
 
     private const string TickersById = BaseUrl + "coins/{0}/tickers?include_exchange_logo=true&order=volume_desc";
 
     private const string Search = BaseUrl + "search?query={0}";
+
+    private const string TOP = "top", COIN = "coin", TICKERS = "tickers", SEARCH = "search";
 
     private readonly HttpClient _client = new()
     {
@@ -31,16 +33,32 @@ public class CoinGeckoApiClient
         }
     };
 
+    public CoinGeckoApiClient()
+    {
+        Cache.Register(TOP, TimeSpan.FromSeconds(45));
+    }
+
+    private CacheService Cache { get; } = new();
+
     /// <summary>
     /// Returns top 20 (by default) currencies ranked by market cap.
     /// </summary>
     public async Task<List<Currency>> GetTopCurrencies(int perPage = 20)
     {
+        if (Cache.IsRelevant<List<Currency>>(TOP, out var data))
+        {
+            return data;
+        }
+
         var url = string.Format(TopCoins, perPage.ToString());
 
         var json = await GetResponse(url).ConfigureAwait(false);
 
-        return JsonConvert.DeserializeObject<List<Currency>>(json)!;
+        var result = JsonConvert.DeserializeObject<List<Currency>>(json)!;
+
+        Cache.Update(TOP, result);
+
+        return result;
     }
 
     /// <summary>
@@ -48,11 +66,22 @@ public class CoinGeckoApiClient
     /// </summary>
     public async Task<CurrencyDetails> GetCurrencyDetails(string id)
     {
+        var key = $"{COIN}.{id}";
+        if (Cache.IsRelevant<CurrencyDetails>(key, out var data))
+        {
+            return data;
+        }
+
         var url = string.Format(CoinById, id);
 
         var json = await GetResponse(url).ConfigureAwait(false);
 
-        return JsonConvert.DeserializeObject<List<CurrencyDetails>>(json)![0];
+        var result = JsonConvert.DeserializeObject<List<CurrencyDetails>>(json)![0];
+
+        Cache.Register(key, TimeSpan.FromSeconds(45));
+        Cache.Update(key, result);
+
+        return result;
     }
 
     /// <summary>
@@ -60,6 +89,12 @@ public class CoinGeckoApiClient
     /// </summary>
     public async Task<List<Ticker>> GetCurrencyTickers(string id)
     {
+        var key = $"{TICKERS}.{id}";
+        if (Cache.IsRelevant<List<Ticker>>(key, out var data))
+        {
+            return data;
+        }
+
         var url = string.Format(TickersById, id);
 
         var json = await GetResponse(url).ConfigureAwait(false);
@@ -72,9 +107,12 @@ public class CoinGeckoApiClient
             ticker.Volume24H = (decimal)jt.SelectToken("converted_volume.usd")!;
 
             return ticker;
-        });
+        }).ToList();
 
-        return tickers.ToList();
+        Cache.Register(key, TimeSpan.FromMinutes(2));
+        Cache.Update(key, tickers);
+
+        return tickers;
     }
 
     /// <summary>
@@ -82,13 +120,22 @@ public class CoinGeckoApiClient
     /// </summary>
     public async Task<List<CoinSearchResult>> SearchCoins(string query)
     {
+        var key = $"{SEARCH}.{query}";
+        if (Cache.IsRelevant<List<CoinSearchResult>>(key, out var data))
+        {
+            return data;
+        }
+
         var url = string.Format(Search, query);
 
         var json = await GetResponse(url).ConfigureAwait(false);
 
-        var coins = SelectJToken(json, "coins").Select(jt => jt.ToObject<CoinSearchResult>()!);
+        var coins = SelectJToken(json, "coins").Select(jt => jt.ToObject<CoinSearchResult>()!).ToList();
 
-        return coins.ToList();
+        Cache.Register(key, TimeSpan.FromMinutes(15));
+        Cache.Update(key, coins);
+
+        return coins;
     }
 
     /// <summary> Wraps the process making an HTTP GET-request. </summary>
