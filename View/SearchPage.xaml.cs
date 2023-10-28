507e277c288dd375958498b36c1cd7548e7c00dc
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2023-10-28 17:05:35 +0300

    Fixed switching tabs with hotkeys on the search page

--- View/SearchPage.xaml.cs
+++ View/SearchPage.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace CoinTrack.View;
 
@@ -7,5 +8,16 @@ public partial class SearchPage : Page
     public SearchPage()
     {
         InitializeComponent();
+    }
+
+    /// <summary>
+    /// Is used to fix switching tabs when the searchbar is focused
+    /// </summary>
+    private void SearchBar_OnPreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.KeyboardDevice.Modifiers == ModifierKeys.Alt)
+        {
+            Scroll.Focus();
+        }
     }
 }
