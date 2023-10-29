16e25a29313ba6ee6d0e0a14d67c420dc2296bc4
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2023-10-29 05:41:09 +0200

    Light / Dark Theme

--- Converters/PercentToColorConverter.cs
+++ Converters/PercentToColorConverter.cs
@@ -1,4 +1,5 @@
 using System.Windows.Media;
+using CoinTrack.Services;
 
 namespace CoinTrack.Converters;
 
@@ -6,11 +7,15 @@ public class PercentToColorConverter : OneWayConverter<decimal?>
 {
     private readonly SolidColorBrush Red = new(Color.FromRgb(255, 58, 51));
     private readonly SolidColorBrush Green = new(Color.FromRgb(0, 168, 62));
-    private readonly SolidColorBrush Gray = new(Color.FromRgb(74, 74, 74));
+    private readonly SolidColorBrush GrayL = new(Color.FromRgb(33, 37, 41));
+    private readonly SolidColorBrush GrayD = new(Color.FromRgb(230, 240, 250));
 
     protected override object Convert(decimal? value)
     {
-        if (value is null) return Gray;
+        if (value is null)
+        {
+            return AppServices.MainWindow.ActiveTheme.ToString().Contains("Dark") ? GrayD : GrayL;
+        }
 
         return value < 0 ? Red : Green;
     }
--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
 using CoinTrack.Helpers;
 using CoinTrack.Services;
@@ -16,8 +19,11 @@ public class MainWindowViewModel : NotifyPropertyChanged
 
         TabBar = new TabBar(new ObservableCollection<Page>() { mainPage });
 
+        SwitchThemes();
+
         OpenMainPage = new RelayCommand(_ => TabBar.NewTab(mainPage));
         OpenSearchPage = new RelayCommand(_ => TabBar.NewTab(new SearchPage()));
+        SwitchTheme = new RelayCommand(_ => SwitchThemes());
     }
 
     public TabBar TabBar { get; }
@@ -26,4 +32,23 @@ public class MainWindowViewModel : NotifyPropertyChanged
     public RelayCommand OpenMainPage { get; }
 
     public RelayCommand OpenSearchPage { get; }
+
+    public RelayCommand SwitchTheme { get; }
+
+    private List<Uri> Themes { get; } = new()
+    {
+        new Uri("Themes/Light.xaml", UriKind.Relative),
+        new Uri("Themes/Dark.xaml", UriKind.Relative)
+    };
+
+    public Uri ActiveTheme { get; set; } = null!;
+
+    private void SwitchThemes()
+    {
+        ActiveTheme = Themes[(Themes.IndexOf(ActiveTheme) + 1) % Themes.Count];
+
+        var theme = new ResourceDictionary() { Source = ActiveTheme };
+
+        Application.Current.Resources.MergedDictionaries[0] = theme;
+    }
 }
